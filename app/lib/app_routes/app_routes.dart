451f62940e6693a23243f082ec3bb7895fc39a02
Leta-D <letadeju.1278@gmail.com>
2026-01-23 21:09:00 +0300

    flutter file setup

---
+++ app/lib/app_routes/app_routes.dart
@@ -0,0 +1,24 @@
+class AppRoutes {
+  static const String settings = '/settings';
+  static const String actorSelection = '/actorSelection';
+  static const String login = '/signin';
+  static const String signup = '/signup';
+
+  static const String mainPage = '/mainPage';
+  static const String profilePage = '/profilePage';
+  // static const String notificationPage = '/notificationPage';
+  static const String detailedItemView = '/detailedItemView';
+
+  static const String donatePage = '/donatePage';
+  static const String myDonationPage = '/myDonationPage';
+  static const String donorHomePage = '/donorHomePage';
+
+  static const String findDonationsPage = '/findDonationsPage';
+  static const String mapPage = '/mapPage';
+  static const String ngoHomePage = '/ngoHomePage';
+  static const String splashScreen = '/splashScreen';
+
+  // static const String accepted = '/acceptedPage';
+}
+
+enum AppRouteAnimationType { fade, scale, slide, rotation }
---
+++ app/lib/app_routes/routes_generator.dart
@@ -0,0 +1,163 @@
+import 'package:capstone_project/app_routes/app_routes.dart';
+import 'package:capstone_project/constant/app_colors.dart';
+import 'package:capstone_project/ui/authen/login_page.dart';
+import 'package:capstone_project/ui/authen/signup_page.dart';
+import 'package:capstone_project/ui/landing_pages/actor_selection.dart';
+import 'package:capstone_project/ui/landing_pages/splash_screen.dart';
+import 'package:capstone_project/ui/main/common/detailed_item_view.dart';
+import 'package:capstone_project/ui/main/common/main_page.dart';
+import 'package:capstone_project/ui/main/common/profile_page.dart';
+import 'package:capstone_project/ui/main/donor/donate_page.dart';
+import 'package:capstone_project/ui/main/donor/donor_home_page.dart';
+import 'package:capstone_project/ui/main/donor/my_donations_page.dart';
+import 'package:capstone_project/ui/main/ngo/find_donations_page.dart';
+import 'package:capstone_project/ui/main/ngo/map_page.dart';
+import 'package:capstone_project/ui/main/ngo/ngo_home_page.dart';
+import 'package:flutter/material.dart';
+
+class RoutesGenerator {
+  static Route<dynamic> generateRoute(RouteSettings settings) {
+    final args = settings.arguments as Map<String, dynamic>?;
+
+    switch (settings.name) {
+      case AppRoutes.actorSelection:
+        return _animatedRoute(
+          ActorSelection(),
+          args?["animation"],
+          args?["duration"],
+        );
+
+      case AppRoutes.login:
+        return _animatedRoute(
+          LoginPage(),
+          args?["animation"],
+          args?["duration"],
+        );
+
+      case AppRoutes.signup:
+        return _animatedRoute(
+          SignupPage(),
+          args?["animation"],
+          args?["duration"],
+        );
+
+      case AppRoutes.mainPage:
+        return _animatedRoute(
+          MainPage(),
+          args?["animation"],
+          args?["duration"],
+        );
+
+      case AppRoutes.donorHomePage:
+        return _animatedRoute(
+          DonorHomePage(),
+          args?["animation"],
+          args?["duration"],
+        );
+
+      case AppRoutes.ngoHomePage:
+        return _animatedRoute(
+          NgoHomePage(),
+          args?["animation"],
+          args?["duration"],
+        );
+
+      case AppRoutes.profilePage:
+        return _animatedRoute(
+          ProfilePage(),
+          args?["animation"],
+          args?["duration"],
+        );
+
+      case AppRoutes.detailedItemView:
+        return _animatedRoute(
+          DetailedItemView(item: args?["selected_item"]),
+          args?["animation"],
+          args?["duration"],
+        );
+
+      case AppRoutes.mapPage:
+        return _animatedRoute(MapPage(), args?["animation"], args?["duration"]);
+
+      case AppRoutes.donatePage:
+        return _animatedRoute(
+          DonatePage(),
+          args?["animation"],
+          args?["duration"],
+        );
+
+      case AppRoutes.myDonationPage:
+        return _animatedRoute(
+          MyDonationsPage(),
+          args?["animation"],
+          args?["duration"],
+        );
+
+      case AppRoutes.findDonationsPage:
+        return _animatedRoute(
+          FindDonationsPage(),
+          args?["animation"],
+          args?["duration"],
+        );
+
+      // case AppRoutes.notificationPage:
+      //   return _animatedRoute(
+      //     NotificationsPage(),
+      //     args?["animation"],
+      //     args?["duration"],
+      //   );
+
+      case AppRoutes.splashScreen:
+        return _animatedRoute(
+          SplashScreen(),
+          args?["animation"],
+          args?["duration"],
+        );
+
+      default:
+        return MaterialPageRoute(
+          builder: (_) => Scaffold(
+            appBar: AppBar(
+              title: Text("Error", style: TextStyle(color: AppColors.red(1))),
+            ),
+            body: Center(child: Text('No route defined for ${settings.name}')),
+          ),
+        );
+    }
+  }
+
+  static _animatedRoute(
+    Widget page,
+    AppRouteAnimationType? type,
+    int? duration,
+  ) {
+    return PageRouteBuilder(
+      pageBuilder: (context, animation, __) => page,
+      transitionDuration: Duration(milliseconds: duration ?? 300),
+      transitionsBuilder: (context, animation, __, child) {
+        switch (type) {
+          case AppRouteAnimationType.fade:
+            return FadeTransition(opacity: animation, child: child);
+
+          case AppRouteAnimationType.scale:
+            return ScaleTransition(scale: animation, child: child);
+
+          case AppRouteAnimationType.slide:
+            return SlideTransition(
+              position: Tween(
+                begin: const Offset(1, 1),
+                end: Offset.zero,
+              ).animate(animation),
+              child: child,
+            );
+
+          case AppRouteAnimationType.rotation:
+            return RotationTransition(turns: animation, child: child);
+
+          default:
+            return FadeTransition(opacity: animation, child: child);
+        }
+      },
+    );
+  }
+}
